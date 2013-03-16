4983dc4387accc1f937da18561092d0dd05f0173
scribu <mail@scribu.net>
2013-03-16 05:13:03 +0200

    convert code from forked plugin

---
+++ wp-super-cache-cli.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: WP Super Cache CLI
+Version: 1.0
+Description: A CLI interface for the WP Super Cache plugin
+Author: WP-CLI Team
+Author URI: http://github.com/wp-cli
+Plugin URI: http://github.com/wp-cli/wp-super-cache-cli
+License: MIT
+*/
+
+function wp_super_cache_cli_init() {
+	if ( !function_exists( 'wp_super_cache_enable' ) )
+		return;
+
+	if ( defined('WP_CLI') && WP_CLI ) {
+		WP_CLI::add_man_dir( dirname(__FILE__) . '/man', dirname(__FILE__) . '/man' );
+
+		include dirname(__FILE__) . '/cli.php';
+	}
+}
+add_action( 'plugins_loaded', 'wp_super_cache_cli_init' );
+
