3c32a50d2b37b34a1e8400f0ba5884622d099ac1
Ian Dunn <ian@iandunn.name>
2013-12-15 13:37:20 -0800

    Add preload command

--- cli.php
+++ cli.php
@@ -93,6 +93,76 @@ class WPSuperCache_Command extends WP_CLI_Command {
 			WP_CLI::error('The WP Super Cache is still enabled, check its settings page for more info.');
 		}
 	}
+
+	/**
+	 * Primes the cache by creating static pages before users visit them
+	 *
+	 * @synopsis [--status] [--cancel]
+	 */
+	function preload( $args = array(), $assoc_args = array() ) {
+		global $super_cache_enabled;
+		$preload_counter = get_option( 'preload_cache_counter' );
+		$preloading      = is_array( $preload_counter ) && $preload_counter['c'] > 0;
+		$pending_cancel  = get_option( 'preload_cache_stop' );
+			
+		// Bail early if caching or preloading is disabled
+		if( ! $super_cache_enabled ) {
+			WP_CLI::error( 'The WP Super Cache is not enabled.' );
+		}
+
+		if ( defined( 'DISABLESUPERCACHEPRELOADING' ) && true == DISABLESUPERCACHEPRELOADING ) {
+			WP_CLI::error( 'Cache preloading is not enabled.' );
+		}
+
+		// Display status
+		if ( isset( $assoc_args['status'] ) ) {
+			$this->preload_status( $preload_counter, $pending_cancel );
+			exit();
+		}
+
+		// Cancel preloading if in progress
+		if ( isset( $assoc_args['cancel'] ) ) {
+			if ( $preloading ) {
+				if ( $pending_cancel ) {
+					WP_CLI::error( 'There is already a pending preload cancel. It may take up to a minute for it to cancel completely.' );
+				} else {
+					update_option( 'preload_cache_stop', true );
+					WP_CLI::success( 'Scheduled preloading of cache almost cancelled. It may take up to a minute for it to cancel completely.' );
+					exit();
+				}
+			} else {
+				WP_CLI::error( 'Not currently preloading.' );
+			}
+		}
+		 
+		// Start preloading if not already in progress
+		if ( $preloading ) {
+			WP_CLI::warning( 'Cache preloading is already in progress.' );
+			$this->preload_status( $preload_counter, $pending_cancel );
+			exit();
+		} else {
+			wp_schedule_single_event( time(), 'wp_cache_full_preload_hook' );
+			WP_CLI::success( 'Scheduled preload for next cron run.' );
+		}
+	}
+
+	/**
+	 * Outputs the status of preloading
+	 *
+	 * @param $preload_counter
+	 * @param $pending_cancel
+	 */
+	protected function preload_status( $preload_counter, $pending_cancel ) {
+		if ( is_array( $preload_counter ) && $preload_counter['c'] > 0 ) {
+			WP_CLI::line( sprintf( 'Currently caching from post %d to %d.', $preload_counter[ 'c' ] - 100, $preload_counter[ 'c' ] ) );
+			
+			if ( $pending_cancel ) {
+				WP_CLI::warning( 'Pending preload cancel. It may take up to a minute for it to cancel completely.' );
+			}
+		} else {
+			WP_CLI::line( 'Not currently preloading.' );
+		}
+	}
 }
 
 WP_CLI::add_command( 'super-cache', 'WPSuperCache_Command' );
