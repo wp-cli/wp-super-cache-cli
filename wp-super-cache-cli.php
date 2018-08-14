5db1e2f7f409d71519907e67411869fb9d79c023
Wojciech Smoliński <wojsmol@wp.pl>
2018-08-14 21:18:13 +0200

    remove manual require

--- wp-super-cache-cli.php
+++ wp-super-cache-cli.php
@@ -9,10 +9,6 @@ Plugin URI: http://github.com/wp-cli/wp-super-cache-cli
 License: MIT
 */
 
-if ( defined( 'WP_CLI' ) && WP_CLI ) {
-	require_once dirname( __FILE__ ) . '/cli.php';
-}
-
 $autoload = __DIR__ . '/vendor/autoload.php';
 if ( file_exists( $autoload ) ) {
 	require_once $autoload;
